3bd72d5e84c73abcd79447b32377b257e73d8086
DmitryBahtenkov <cool.iridiy12soul@gmail.com>
2022-01-02 17:59:52 +0300

    add dna linear search

---
+++ SearchTasks/dna_search.py
@@ -0,0 +1,26 @@
+from enum import IntEnum
+from typing import Tuple, List, Type
+
+Nucleotide: IntEnum = IntEnum('Nucleotide', ('A', 'C', 'G', 'T'))
+Codon = Tuple[Nucleotide, Nucleotide, Nucleotide]
+Gene = List[Codon]
+
+gene_str: str = 'ACGTTGCGCTAGATCGCTAGCTAGCTGCGCGCTAGCGCATGAGCATCGCATAGCGCTAGAGCATCG' * 100
+
+
+def string_to_gene(s: str) -> Gene:
+    gene: Gene = []
+    for i in range(0, len(s), 3):
+        if (i + 2) >= len(s):
+            return gene
+        codon: Codon = (Nucleotide[s[i]], Nucleotide[s[i + 1]], Nucleotide[s[i + 2]])
+        gene.append(codon)
+
+    return gene
+
+
+def linear_contains(gene: Gene, key_codon: Codon) -> bool:
+    for codon in gene:
+        if codon == key_codon:
+            return True
+    return False
---
+++ Tests/SearchTasks/dna_search_tests.py
@@ -0,0 +1,17 @@
+import unittest
+from SearchTasks.dna_search import *
+
+
+class DnaSearchTestCase(unittest.TestCase):
+    def test_contains(self):
+        gene_str: str = 'ACGTTGCGCTAGATCGCTAGCTAGCTGCGCGCTAGCGCATGAGCATCGCATAGCGCTAGAGCATCG' * 100
+        acg: Codon = (Nucleotide.A, Nucleotide.C, Nucleotide.G)
+        self.assertTrue(linear_contains(string_to_gene(gene_str), acg))
+
+    def test_not_contains(self):
+        gene_str: str = 'AAAA' * 100
+        acg: Codon = (Nucleotide.A, Nucleotide.C, Nucleotide.G)
+        self.assertEqual(linear_contains(string_to_gene(gene_str), acg), False)
+
+if __name__ == '__main__':
+    unittest.main()
