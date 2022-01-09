98a8d866067f9a2e93b9f43c7e2d631826eda3da
DmitryBahtenkov <cool.iridiy12soul@gmail.com>
2022-01-09 11:59:39 +0300

    add main to map_coloring

--- CSPTasks/map_coloring.py
+++ CSPTasks/map_coloring.py
@@ -14,3 +14,29 @@ class MapColoringConstraint(Constraint[str, str]):
             return True
         
         return assigment[self.place1] != assigment[self.place2]
+
+
+def main():
+    variables: List[str] = ['Western A', 'Northern T', 'South A', 'Queensland', 'New South Wales', 'Victoria', 'Tasmania']
+    domains: Dict[str, List[str]] = {}
+
+    for variable in variables:
+        domains[variable] = ['red', 'green', 'blue']
+    
+    csp: CSP[str, str] = CSP(variables, domains)
+    csp.add_constraint(MapColoringConstraint(variables[0], variables[1]))
+    csp.add_constraint(MapColoringConstraint(variables[0], variables[2]))
+    csp.add_constraint(MapColoringConstraint(variables[2], variables[1]))
+    csp.add_constraint(MapColoringConstraint(variables[3], variables[1]))
+    csp.add_constraint(MapColoringConstraint(variables[3], variables[2]))
+    csp.add_constraint(MapColoringConstraint(variables[3], variables[4]))
+    csp.add_constraint(MapColoringConstraint(variables[4], variables[2]))
+    csp.add_constraint(MapColoringConstraint(variables[5], variables[2]))
+    csp.add_constraint(MapColoringConstraint(variables[5], variables[4]))
+    csp.add_constraint(MapColoringConstraint(variables[5], variables[6]))
+
+    solution: Optional[Dict[str, str]] = csp.backtracking_search()
+    if solution is None:
+        print('No sulution')
+    else:
+        print(solution)
--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-from SearchTasks.missionaries import *
+from CSPTasks.map_coloring import *
+from CSPTasks.csp import *
 
 if __name__ == '__main__':
-    
-    start: MCState = MCState(MAX_NUM, MAX_NUM, True)
-    print(start)
-    print('____________')
-    
-    solution: Optional[Node[MCState]] = bfs(start, MCState.goal_test, MCState.successors)
-
-    if solution is None:
-        print("No solution")
-    else:
-        path: List[MCState] = node_to_path(solution)
-        display_solution(path)
+    main()
+        
